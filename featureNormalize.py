d65d691012dc6127efddbfb1325a70503a2ba8b8
Sarthak Joshi <sarthak.joshi@innovaccer.com>
2017-03-27 15:39:09 +0530

    fixes

--- featureNormalize.py
+++ featureNormalize.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 def normalize(featureMat):
-    mean = np.mean(featureMat, axis=0)
-    std = np.std(featureMat, axis=0, ddof=1)
-    return (featureMat-mean)/std
+    mu = np.mean(featureMat, axis=0)
+    sigma = np.std(featureMat, axis=0, ddof=1)
+    return (featureMat-mu)/sigma
